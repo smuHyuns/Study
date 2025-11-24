eea81c897a3b5812f4f6398bf3a16ce8d4a13491
최현수 <106013493+smuHyuns@users.noreply.github.com>
2025-11-24 22:16:26 +0900

    feat : Redis hashOperation 활용 데이터 저장

--- project/src/main/java/study/project/controller/RedisTestController.java
+++ project/src/main/java/study/project/controller/RedisTestController.java
@@ -2,18 +2,24 @@ package study.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import study.project.repository.TestCacheRepository;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 public class RedisTestController {
+    private final TestCacheRepository testCacheRepository;
 
-    @GetMapping()
-    ResponseEntity<String> testRedis() {
-        return ResponseEntity.ok("test");
+    @GetMapping("/hash")
+    ResponseEntity<String> testRedis(@RequestParam("username") String username, @RequestParam("column") String column) {
+        String columnValue = testCacheRepository.findColumn(username, column);
+        return ResponseEntity.ok("column : " + columnValue);
+    }
+
+    @PostMapping("/hash/save")
+    void saveHashTestData(@RequestBody String username) {
+        testCacheRepository.setUpHash(username);
     }
 }
